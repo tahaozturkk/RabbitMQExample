44d64143aac1caae5e51f40aaf6df6616371e025
tahaozturkk <ozturktaha1453@gmail.com>
2023-01-25 22:19:32 +0300

    Add project files.

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQConsumer
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("your_AMQP_URL");
+
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("messageQueue", false, false, false);
+                EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                channel.BasicConsume("messageQueue", false, consumer);
+                consumer.Received += (sender, e) =>
+                {
+                   
+                    Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+                };
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ RabbitMQPublisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("your_AMQP_URL");
+            //factory.HostName = "localhost";
+
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("messageQueue", false, false, false);
+                byte[] bytemessage = Encoding.UTF8.GetBytes("MessageContent");
+                channel.BasicPublish(exchange: "", routingKey: "messageQueue", body: bytemessage);
+            }
+        }
+    }
+}
